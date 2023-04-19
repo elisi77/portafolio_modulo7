714e4c424daaa2ae6f689fcccbee7e658a5db375
elisi77 <121964742+elisi77@users.noreply.github.com>
2023-04-19 19:47:38 -0400

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,7 @@
+ENTREGA PORTAFOLIO M7
+
+Cuenta Github: https://github.com/elisi77
+
+Link Github: https://github.com/elisi77/portafolio_modulo7
+
+
---
+++ src/routes/ventas.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+const router = Router();
+import { createOrden ,getVentas} from '../controllers/ventas.controllers.js';
+router.get("/api/ventas",getVentas, (req, res) => {})
+.post("/api/ventas",createOrden ,(req, res) => {})
+
+//add product to cart
+
+export default router;
+
+
